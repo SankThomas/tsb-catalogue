02913ad804dcad8a4671b6cde35e4eab4423c639
Thomas Sankara <thomassankara74@gmail.com>
2021-07-21 11:48:29 +0300

    Sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,9 @@
+import { FaHome } from "react-icons/fa"
+
+export default function Sidebar() {
+  return (
+    <aside className="sidebar left-0 top-0 h-full w-10 md:block">
+      <FaHome />
+    </aside>
+  )
+}
