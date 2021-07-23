597a1609c0fe08a0deebeb05e2a7617acd6d635d
Thomas Sankara <thomassankara74@gmail.com>
2021-07-23 10:19:08 +0300

    add absolute positioning

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,12 @@
 import { FaHome } from "react-icons/fa"
+import { Link } from "react-router-dom"
 
 export default function Sidebar() {
   return (
-    <aside className="sidebar p-6 left-0 top-0 h-screen w-20 hidden md:block">
-      <FaHome className="text-2xl" />
+    <aside className="sidebar absolute p-6 left-0 top-18 h-screen w-20 hidden md:block">
+      <Link to="/">
+        <FaHome className="text-2xl" />
+      </Link>
     </aside>
   )
 }
