a605fa2daf44b4b2bf726040d38b26890f4cdec2
Thomas Sankara <thomassankara74@gmail.com>
2021-07-23 11:58:07 +0300

    increase Sidebar z-index

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 export default function Sidebar() {
   return (
-    <aside className="sidebar absolute p-6 left-0 top-18 h-screen w-20 hidden md:block">
+    <aside className="sidebar absolute p-6 left-0 top-18 z-10 h-screen w-20 hidden md:block">
       <Link to="/">
         <FaHome className="text-2xl" />
       </Link>
