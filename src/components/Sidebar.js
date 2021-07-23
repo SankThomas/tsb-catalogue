fe21b9ed592f55a1b39f2396c2ac9a6a39da5a98
Thomas Sankara <thomassankara74@gmail.com>
2021-07-23 10:15:33 +0300

    increase padding and icon size

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,8 +2,8 @@ import { FaHome } from "react-icons/fa"
 
 export default function Sidebar() {
   return (
-    <aside className="sidebar left-0 top-0 h-full w-20 hidden md:block">
-      <FaHome />
+    <aside className="sidebar p-6 left-0 top-0 h-screen w-20 hidden md:block">
+      <FaHome className="text-2xl" />
     </aside>
   )
 }
