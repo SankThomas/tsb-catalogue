0695403c31ee4a0e8f3967bad3a499f93d6b122f
Thomas Sankara <thomassankara74@gmail.com>
2021-07-23 10:14:15 +0300

    increase Sidebar width

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,7 @@ import { FaHome } from "react-icons/fa"
 
 export default function Sidebar() {
   return (
-    <aside className="sidebar left-0 top-0 h-full w-10 md:block">
+    <aside className="sidebar left-0 top-0 h-full w-20 hidden md:block">
       <FaHome />
     </aside>
   )
